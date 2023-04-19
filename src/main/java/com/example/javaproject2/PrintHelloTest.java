9d3ca627e879e654966f0e1ce632436568aece81
Lim ji yang <fluffy8854@naver.com>
2023-04-19 11:21:21 +0900

    feat: PrintHello, PrintHelloTest 작성

---
+++ src/main/java/com/example/javaproject2/PrintHelloTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello p = new PrintHello();
+        p.print();
+    }
+}
