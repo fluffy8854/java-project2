97eec27239d0df61ab1fc4f72529842b70799916
Lim ji yang <fluffy8854@naver.com>
2023-04-20 11:30:03 +0900

    feat: MultiplicationTale, Code10 작성

---
+++ src/main/java/com/example/javaproject2/codeup/Code10.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Code10 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        System.out.println(n);
+    }
+}
