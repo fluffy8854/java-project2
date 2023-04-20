57aa748db19cbeb526b87e09f48afb5cb1862097
Lim ji yang <fluffy8854@naver.com>
2023-04-20 09:31:14 +0900

    feat: Scanner 복습

--- src/main/java/com/example/javaproject2/codeup/Code5.java
+++ src/main/java/com/example/javaproject2/codeup/Code5.java
@@ -2,6 +2,6 @@ package com.example.javaproject2.codeup;
 
 public class Code5 {
     public static void main(String[] args) {
-        System.out.println("\"Hello World\"");
+        System.out.println("\"Hello World\""); // 백슬래시
     }
 }
--- src/main/java/com/example/javaproject2/week1/ScannerEx.java
+++ src/main/java/com/example/javaproject2/week1/ScannerEx.java
@@ -3,12 +3,10 @@ package com.example.javaproject2.week1;
 import java.util.Scanner;
 
 public class ScannerEx {
-    public void plusTowNumbers(int val1, int val2){
-
-    }
 
     public void readTwoNumbersAndPlus(){
         Scanner sc = new Scanner(System.in);
+        // 두개의 입력을 받아 출력하기
         System.out.println(sc.nextInt() + sc.nextInt());
     }
 }
