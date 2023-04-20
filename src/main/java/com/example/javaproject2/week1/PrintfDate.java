a183c39f383573abfca56c13f0b2533b07e481a5
Lim ji yang <fluffy8854@naver.com>
2023-04-20 11:49:10 +0900

    feat: PrintfDate 작성

---
+++ src/main/java/com/example/javaproject2/week1/PrintfDate.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1;
+
+public class PrintfDate {
+    public static void main(String[] args) {
+        System.out.printf("%02d\n",4);
+        // 2023-04-20
+        System.out.printf("%d-%02d-%02d",2023,04,20);
+    }
+}
