f58f2f286f5d73a2f2ae738ae280388b640253de
Lim ji yang <fluffy8854@naver.com>
2023-04-20 11:19:13 +0900

    feat: .printfEx() 작성

---
+++ src/main/java/com/example/javaproject2/week1/PrintfEx.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1;
+
+public class PrintfEx {
+    public void print(){
+        System.out.printf("%s","Hello");
+    }
+
+    public static void main(String[] args) {
+        PrintfEx pf = new PrintfEx();
+        pf.print();
+    }
+}
