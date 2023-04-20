645a0b40e7323cae9491020f5dc2900b071ff768
Lim ji yang <fluffy8854@naver.com>
2023-04-20 09:39:41 +0900

    feat: ScannerStrEX, ScannerStrTest 작성

---
+++ src/main/java/com/example/javaproject2/week1/ScannerStrTest.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1;
+
+public class ScannerStrTest {
+    public static void main(String[] args) {
+
+        ScannerStrEx ss = new ScannerStrEx();
+        ss.inputStringsAndPlus();
+    }
+}
