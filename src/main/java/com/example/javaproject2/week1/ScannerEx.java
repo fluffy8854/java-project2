55abb49836e91e93f4a9dd1885edcec0f1935201
Lim ji yang <fluffy8854@naver.com>
2023-04-20 14:37:39 +0900

    fix: ScannerEx 변수 활용 수정

--- src/main/java/com/example/javaproject2/week1/ScannerEx.java
+++ src/main/java/com/example/javaproject2/week1/ScannerEx.java
@@ -9,11 +9,11 @@ public class ScannerEx {
         // 두개의 입력을 받아 출력하기
         System.out.println("숫자 두 개를 입력해주세요.");
         System.out.printf("첫 번째 숫자 : ");
-        //int n1 = sc.nextInt();
-        System.out.println("입력하신 첫 번째 숫자 : "+ sc.nextInt());
+        int n1 = sc.nextInt();
+        System.out.println("입력하신 첫 번째 숫자 : "+ n1);
         System.out.printf("두 번째 숫자 : ");
-        //int n2 = sc.nextInt();
-        System.out.println("입력하신 두 번째 숫자 : "+ sc.nextInt());
-        //System.out.println("두 수의 합 : "+ (n1+n2));
+        int n2 = sc.nextInt();
+        System.out.println("입력하신 두 번째 숫자 : "+ n2);
+        System.out.println("두 수의 합 : "+ (n1+n2));
     }
 }
