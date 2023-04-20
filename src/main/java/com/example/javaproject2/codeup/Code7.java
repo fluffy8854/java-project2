698ec61fd27ac832847ee36c8ae38c551501dc8b
Lim ji yang <fluffy8854@naver.com>
2023-04-20 10:43:20 +0900

    feat: .print() 작성

--- src/main/java/com/example/javaproject2/codeup/Code7.java
+++ src/main/java/com/example/javaproject2/codeup/Code7.java
@@ -2,7 +2,7 @@ package com.example.javaproject2.codeup;
 
 public class Code7 {
     public static void main(String[] args) {
-
+        // 따옴표, 백슬래시 모두 백슬래시를 앞에 넣어서 출력
         System.out.println("\"C:\\Download\\hello.cpp\"");
     }
 }
--- src/main/java/com/example/javaproject2/week1/ScannerEx.java
+++ src/main/java/com/example/javaproject2/week1/ScannerEx.java
@@ -7,6 +7,13 @@ public class ScannerEx {
     public void readTwoNumbersAndPlus(){
         Scanner sc = new Scanner(System.in);
         // 두개의 입력을 받아 출력하기
-        System.out.println(sc.nextInt() + sc.nextInt());
+        System.out.println("숫자 두 개를 입력해주세요.");
+        System.out.printf("첫 번째 숫자 : ");
+        //int n1 = sc.nextInt();
+        System.out.println("입력하신 첫 번째 숫자 : "+ sc.nextInt());
+        System.out.printf("두 번째 숫자 : ");
+        //int n2 = sc.nextInt();
+        System.out.println("입력하신 두 번째 숫자 : "+ sc.nextInt());
+        //System.out.println("두 수의 합 : "+ (n1+n2));
     }
 }
