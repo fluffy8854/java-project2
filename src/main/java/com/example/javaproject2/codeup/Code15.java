5199f2faeec68e3a8569cd91c9fdcb3fceb4f50a
Lim ji yang <fluffy8854@naver.com>
2023-04-20 15:48:40 +0900

    feat: Codeup 15 작성

---
+++ src/main/java/com/example/javaproject2/codeup/Code15.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Code15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 소숫점 자리수 .~ 으로 제한
+        System.out.printf("%.2f",sc.nextFloat());
+    }
+}
