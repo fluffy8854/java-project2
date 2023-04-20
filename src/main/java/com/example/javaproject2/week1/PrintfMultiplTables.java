309bcd290c18191f130a8d29f1699240832afebe
Lim ji yang <fluffy8854@naver.com>
2023-04-20 13:15:09 +0900

    feat: PrintfMutiplTables 작성

---
+++ src/main/java/com/example/javaproject2/week1/PrintfMultiplTables.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1;
+
+public class PrintfMultiplTables {
+    public static void main(String[] args) {
+        for (int i = 1; i<10 ;i++){
+            System.out.printf("%d x %d = %d\n",2,i,2*i);
+        }
+
+    }
+}
