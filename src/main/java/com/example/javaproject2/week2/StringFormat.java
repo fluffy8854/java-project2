e555334971f39efc925d949e5fc21db5ea11ed68
Lim ji yang <fluffy8854@naver.com>
2023-04-25 13:35:17 +0900

    feat: StringSplit 작성

--- src/main/java/com/example/javaproject2/week2/StringFormat.java
+++ src/main/java/com/example/javaproject2/week2/StringFormat.java
@@ -7,5 +7,6 @@ public class StringFormat {
         // printf의 출력 처럼 나오는 문자열을 반환해주는 함수 String.format
         String message = String.format(" %s님의 통장잔고는 %d원 입니다", name,balance);
         System.out.println(message);
+
         }
 }
