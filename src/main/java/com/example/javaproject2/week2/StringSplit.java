6fbe6a500d9e510d2120b6c6acfcab8d76ff7d37
Lim ji yang <fluffy8854@naver.com>
2023-04-25 13:35:22 +0900

    feat: StringSplit 작성

---
+++ src/main/java/com/example/javaproject2/week2/StringSplit.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week2;
+
+public class StringSplit {
+    public static void main(String[] args) {
+        String str = "11 3 65 778 9544";
+        String[] arr = str.split(" ");
+
+        for(int i = 0 ; i < arr.length ; i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
