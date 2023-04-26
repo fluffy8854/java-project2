bbbb46ab18da0a8c29a00ef91e622732934c20f6
Lim ji yang <fluffy8854@naver.com>
2023-04-26 15:35:32 +0900

    feat: LikeAccumulate 작성

---
+++ src/main/java/com/example/javaproject2/week2/LikeAccumulate.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.week2;
+
+public class LikeAccumulate {
+    public static void main(String[] args) {
+        int likeCount = 0;
+
+        likeCount = likeCount + 1;
+        System.out.println(likeCount);
+
+        likeCount = likeCount + 1;
+        System.out.println(likeCount);
+
+        likeCount = likeCount - 1;
+        System.out.println(likeCount);
+
+
+    }
+}
