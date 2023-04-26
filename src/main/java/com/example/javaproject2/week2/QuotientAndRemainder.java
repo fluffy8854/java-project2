840d63f78e9759c91819e21f8d0c94c7eb3b7eda
Lim ji yang <fluffy8854@naver.com>
2023-04-26 14:13:29 +0900

    feat: QuotientAndRemainder 작성

---
+++ src/main/java/com/example/javaproject2/week2/QuotientAndRemainder.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week2;
+
+public class QuotientAndRemainder {
+    public static void main(String[] args) {
+        System.out.printf("Quotient : %d\n",10 / 3);
+        System.out.printf("Remainer : %d",10 % 3);
+    }
+}
