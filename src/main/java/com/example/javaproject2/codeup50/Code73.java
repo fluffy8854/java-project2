9364b8f3db949534468668f0bbbbe35b72b03b61
Lim ji yang <fluffy8854@naver.com>
2023-04-28 13:39:03 +0900

    feat: SwitchCaseSeason3, SwitchCaseClinicHours 작성

---
+++ src/main/java/com/example/javaproject2/codeup50/Code73.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.codeup50;
+
+import java.util.Scanner;
+
+public class Code73 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        while(num != 0){
+            System.out.println(num);
+            num = sc.nextInt();
+        }
+    }
+}
