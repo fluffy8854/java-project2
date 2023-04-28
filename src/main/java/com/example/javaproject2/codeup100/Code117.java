9f55a9fad6f7621ecddfb4193c097bb213ec02fb
Lim ji yang <fluffy8854@naver.com>
2023-04-28 14:16:42 +0900

    feat: Code 116~118 작성

---
+++ src/main/java/com/example/javaproject2/codeup100/Code117.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.codeup100;
+
+import java.util.Scanner;
+
+public class Code117 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.printf("%.2f",num1*num2);
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup100/Code118.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.codeup100;
+
+import java.util.Scanner;
+
+public class Code118 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.printf("%.1f",num1*num2/2.0);
+
+    }
+}
