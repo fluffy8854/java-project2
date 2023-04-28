d9abd98db709a23eed386abdc5192b9abb46e606
Lim ji yang <fluffy8854@naver.com>
2023-04-28 11:35:41 +0900

    feat: SwintchCaseEXBreak 작성

---
+++ src/main/java/com/example/javaproject2/week2/SwintchCaseEXBreak.java
@@ -0,0 +1,34 @@
+package com.example.javaproject2.week2;
+
+public class SwintchCaseEXBreak {
+    public static void main(String[] args) {
+        int day = 4;
+
+        switch(day){
+            case 1:
+                System.out.println("월요일");
+                break;
+            case 2:
+                System.out.println("화요일");
+                break;
+            case 3:
+                System.out.println("수요일");
+                break;
+            case 4:
+                System.out.println("목요일");
+                break;
+            case 5:
+                System.out.println("금요일");
+                break;
+            case 6:
+                System.out.println("토요일");
+                break;
+            case 7:
+                System.out.println("일요일");
+                break;
+            default:
+                break;
+
+        }
+    }
+}
