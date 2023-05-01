2dd80e842f27b4feca622c99b8fdbc867c4f086d
Lim ji yang <fluffy8854@naver.com>
2023-05-01 11:29:42 +0900

    feat: ForLoopForward1To10, ForRevers 작성

---
+++ src/main/java/com/example/javaproject2/week3/ForLoopForward1To10.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week3;
+
+public class ForLoopForward1To10 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10 ; i++) {
+            //System.out.println(i);
+            System.out.printf("%d ",i);
+        }
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week3/ForRevers.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week3;
+
+public class ForRevers {
+    public static void main(String[] args) {
+        // 틀린 케이스
+
+        /*for (int i = 10; i > 0; i++) {
+            System.out.println(i);
+        }*/
+
+        for(int i = 10 ; i > 0 ; i--){
+            //System.out.println(i);
+            System.out.printf("%d ",i);
+        }
+    }
+}
