8c690b46afed3e4ad34bd1338cef3fa9df97f208
Lim ji yang <fluffy8854@naver.com>
2023-05-01 09:17:49 +0900

    feat: Code 103, 106 작성

---
+++ src/main/java/com/example/javaproject2/codeup100/Code103.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.codeup100;
+
+public class Code103 {
+    public static void main(String[] args) {
+        System.out.println("\"c:\\test\"");
+    }
+
+}
---
+++ src/main/java/com/example/javaproject2/codeup100/Code106.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup100;
+
+public class Code106 {
+    public static void main(String[] args) {
+        int num1 = Integer.MAX_VALUE;
+        int num2 = Integer.MIN_VALUE;
+
+
+        System.out.printf("%d %d",num2,num1);
+    }
+}
