173129dc93005bef1bd42a0a57e0c8611672f0d1
Lim ji yang <fluffy8854@naver.com>
2023-05-02 14:07:14 +0900

    feat: whileSleep, WhileCnt, WhileCntHorizon 작성

---
+++ src/main/java/com/example/javaproject2/week3/WhileCnt.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week3;
+
+public class WhileCnt {
+    public static void main(String[] args) {
+
+        int cnt = 0;
+        while(cnt < 11){
+            System.out.println(cnt++);
+            //System.out.println(cnt);
+            //cnt++;
+        }
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week3/WhileCntHorizon.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week3;
+
+public class WhileCntHorizon {
+    public static void main(String[] args) {
+
+        int cnt = 0;
+        while( cnt < 11){
+            System.out.printf("%d",cnt++);
+        }
+    }
+}
