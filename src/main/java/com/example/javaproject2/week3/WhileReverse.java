ff6832f9e34e495f6d71cd52b863135fd2bb7451
Lim ji yang <fluffy8854@naver.com>
2023-05-02 14:33:05 +0900

    feat: whileReverse, WhileFactorial 작성

---
+++ src/main/java/com/example/javaproject2/week3/WhileReverse.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week3;
+
+public class WhileReverse {
+    public static void main(String[] args) {
+        int cnt =5;
+        while( cnt > 0){
+            System.out.println(cnt--);
+        }
+    }
+}
