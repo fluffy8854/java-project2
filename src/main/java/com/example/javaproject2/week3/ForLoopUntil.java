d74faafcfc54df6a026420806d4177e347955170
Lim ji yang <fluffy8854@naver.com>
2023-05-02 10:49:46 +0900

    feat: ForLoopUntil 작성

---
+++ src/main/java/com/example/javaproject2/week3/ForLoopUntil.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.week3;
+
+public class ForLoopUntil {
+    public static void main(String[] args) {
+
+        /*for(int i = 2 ; i == 1 ; i++){
+            System.out.println(i);
+        }*/
+
+        /*for (int i = 0; 2*i <= 18 ; i++) {
+            System.out.println(i);
+        }*/
+
+        for(int i = 2; i*i <= 50 ; i++){
+            System.out.println(i);
+        }
+    }
+}
