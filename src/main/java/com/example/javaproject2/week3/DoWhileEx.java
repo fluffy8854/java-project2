1b6eb2fc06a15d918e01e191dcc6edab97dae644
Lim ji yang <fluffy8854@naver.com>
2023-05-03 10:34:08 +0900

    feat: DoWhileEx, DoWhileExEven 작성

---
+++ src/main/java/com/example/javaproject2/week3/DoWhileEx.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.week3;
+
+import java.util.Scanner;
+
+public class DoWhileEx {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num ;
+
+        do{
+            num = sc.nextInt();
+            System.out.println(num);
+        }while(num != 0);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week3/DoWhileExEven.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week3;
+
+import java.util.Scanner;
+
+public class DoWhileExEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num ;
+        do{
+            System.out.print("짝수를 입력하세요 : ");
+            num = sc.nextInt();
+        }while(num % 2 != 0);
+
+    }
+}
