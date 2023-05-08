7f5092c0dca6e946cf1487a9941fa164326132f2
Lim ji yang <fluffy8854@naver.com>
2023-05-08 13:40:03 +0900

    feat: UserFactory, UserFactoryTest 작성

---
+++ src/test/java/com/example/javaproject2/week4/UserFactoryTest.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserFactoryTest {
+    public static void main(String[] args) {
+        UserFactory uf =new UserFactory();
+        User user1= uf.getAdultUser();
+        System.out.println(user1.age);
+    }
+
+}
