9964919a7ed2f723dafbd4143fe35ae26b00ec11
Lim ji yang <fluffy8854@naver.com>
2023-05-08 11:08:33 +0900

    feat: SpaceInvaders, SpaceInvadersTest 작성

---
+++ src/main/java/com/example/javaproject2/week4/SpaceInvaders.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week4;
+
+public class SpaceInvaders {
+    int location;
+
+    public void moveLeft(){
+        this.location--;
+    }
+
+    public void moveRight(){
+        this.location++;
+    }
+}
