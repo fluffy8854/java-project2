2e868779bdffdaa7dc3ac83895a98864beff06d0
Lim ji yang <fluffy8854@naver.com>
2023-05-08 09:34:14 +0900

    feat: RightTriangle 작성

--- src/main/java/com/example/javaproject2/week4/RightTriangle.java
+++ src/main/java/com/example/javaproject2/week4/RightTriangle.java
@@ -1,22 +1,16 @@
-package com.example.javaproject2.CodeupEtc;
+package com.example.javaproject2.week4;
 
 import java.util.Scanner;
 
-public class Code1367 {
-
+public class RightTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-
-        for (int i = 1; i <= num; i++) {
-            for (int j = num; j > i; j--) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k < num; k++) {
+        for (int i = 1; i <= num ; i++) {
+            for (int j = 0; j < i ; j++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
-
     }
 }
