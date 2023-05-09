f8ec21fa22399a4d936151d0304aafb389bf92a0
Lim ji yang <fluffy8854@naver.com>
2023-05-09 13:03:48 +0900

    feat: MultiplicationTabel 수정

--- src/main/java/com/example/javaproject2/week3/MultiplicationTabel.java
+++ src/main/java/com/example/javaproject2/week3/MultiplicationTabel.java
@@ -2,8 +2,15 @@ package com.example.javaproject2.week3;
 
 public class MultiplicationTabel {
     public static void main(String[] args) {
+        printMultipleication(2);
+        printMultipleication(3);
+        printMultipleication(5);
+        printMultipleication(7);
+    }
+
+    public static void printMultipleication(int dan){
         for( int i = 1; i <= 9 ; i++){
-            System.out.printf("2 * %d = %d\n", i, 2*i);
+            System.out.printf("%d * %d = %d\n", dan, i, dan*i);
         }
     }
 }
