9db46fc0c24ab1e188625399392b5b1a0c553753
Lim ji yang <fluffy8854@naver.com>
2023-05-10 14:13:07 +0900

    feat: HelloPrinter 수정

--- src/main/java/com/example/javaproject2/week4/HelloPrinter.java
+++ src/main/java/com/example/javaproject2/week4/HelloPrinter.java
@@ -1,9 +1,17 @@
 package com.example.javaproject2.week4;
 
 public class HelloPrinter {
+
     public void print(String message){
         System.out.println(message);
     }
+
+    public void repeatMessage(int n, String message){
+        for (int i = 0; i < n ; i++) {
+            print(message);
+        }
+    }
+
 
     public static void main(String[] args) {
         HelloPrinter hp = new HelloPrinter();
