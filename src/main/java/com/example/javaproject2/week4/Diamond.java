a323efe741add6375a75041b6bfcecb0776acec8
Lim ji yang <fluffy8854@naver.com>
2023-05-10 10:18:10 +0900

    feat: Diamond 수정

--- src/main/java/com/example/javaproject2/week4/Diamond.java
+++ src/main/java/com/example/javaproject2/week4/Diamond.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class Diamond {
 
+    public String makeALine(int h, int i){
+        String str = "";
+        if(i <= h/2 -1){
+            for(int j = (h - 1) / 2 - i ; j > 0 ; j--) str += " ";
+            for (int j = 0; j < 2 * i + 1; j++) str += "*";
+        }
+        else{
+            for (int j = 0; j < i - h / 2; j++) str += " ";
+            for (int j = 2 * h - 2 * i - 1; j > 0; j--) str += "*";
+        }
+
+        return str;
+    }
+
     public void printDiamond(int h) {
         for (int i = 0; i < h; i++) {
 
@@ -32,7 +46,7 @@ public class Diamond {
         Scanner sc = new Scanner(System.in);
         Diamond dm = new Diamond();
         dm.printDiamond(sc.nextInt());
-
+        System.out.println(dm.makeALine(7,0));
 
 
     }
