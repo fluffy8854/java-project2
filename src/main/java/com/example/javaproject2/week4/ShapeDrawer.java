cead07a2fb4d4fa6256f998e498a34421d368292
Lim ji yang <fluffy8854@naver.com>
2023-05-10 15:17:13 +0900

    feat: ShapeDrawer 수정

--- src/main/java/com/example/javaproject2/week4/ShapeDrawer.java
+++ src/main/java/com/example/javaproject2/week4/ShapeDrawer.java
@@ -1,12 +1,7 @@
 package com.example.javaproject2.week4;
 
-public abstract class ShapeDrawer {
+public interface ShapeDrawer {
+    void printShape(int h);
 
-    public void printShape(int h){
-        for (int i = 0; i < h ; i++) {
-            System.out.println(makeALine(h,i));
-        }
-    }
-
-    public abstract String makeALine(int h, int i);
+    String makeALine(int h, int i);
 }
