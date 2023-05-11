75f1ca265056f8c2d3e6f7c6c64be15b25a69999
Lim ji yang <fluffy8854@naver.com>
2023-05-11 13:38:03 +0900

    feat: Code3108 작성

---
+++ src/main/java/com/example/javaproject2/CodeupEtc/Code3108.java
@@ -0,0 +1,97 @@
+package com.example.javaproject2.CodeupEtc;
+
+import java.util.*;
+
+public class Code3108 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<AttendeesList> al = new ArrayList<>();
+        AttendeesList atl = new AttendeesList();
+
+
+        atl.addAStudent(sc,al);
+        Collections.sort(al);
+        atl.printAStudent(sc,al);
+    }
+}
+
+class AttendeesList implements Comparable<AttendeesList>{
+    public String getC() {
+        return c;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    String c;
+    int code;
+    String name;
+
+    public AttendeesList(){
+
+    }
+    public AttendeesList(String c, int code, String name){
+        this.c = c;
+        this.code = code;
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(AttendeesList o) {
+        if( this.code > o.code){
+            return 1;
+        }
+        else if( this.code < o.code){
+            return -1;
+        }
+        else return 0;
+    }
+
+    public List<AttendeesList> addAStudent(Scanner sc, List<AttendeesList> al){
+        int n = sc.nextInt();
+        boolean dupCode = false;
+        String c ,name;
+        int code;
+
+        // 입력 처리
+        for (int i = 0; i < n ; i++) {
+            c = sc.next();
+            code = sc.nextInt();
+            name = sc.next();
+            if(c.equals("I")){
+                for (int j = 0; j < al.size() ; j++) {
+                    if(al.get(j).getCode() == code){
+                        dupCode = true;
+                    }
+                }
+                if(dupCode){
+                    dupCode = false;
+                    continue;
+                }
+                al.add(new AttendeesList(c, code, name));
+            }
+            else if(c.equals("D")){
+                for (int j = 0; j < al.size() ; j++) {
+                    if(al.get(j).getCode() == code ){
+                        al.remove(j);
+                    }
+                }
+            }
+        }
+        return al;
+    }
+
+    public void printAStudent(Scanner sc, List<AttendeesList> al){
+        int index;
+        for (int i = 0; i < 5 ; i++) {
+            index = sc.nextInt();
+            System.out.printf("%d %s\n",al.get(index-1).getCode(),al.get(index-1).getName());
+        }
+    }
+}
--- src/main/java/com/example/javaproject2/week4/ListUser.java
+++ src/main/java/com/example/javaproject2/week4/ListUser.java
@@ -1,39 +1,17 @@
 package com.example.javaproject2.week4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListUser {
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+        User user1 = new User("김미미", "01012345678" , 26);
+        users.add(user1);
+        User user2 = new User("김나나", "01012345679" , 25);
+        users.add(user2);
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    private String name;
-    private String phoneNumber;
-    private int age;
-
-    public ListUser(String name, String phoneNumber, int age){
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.age = age;
-    }
-
-    public String getName() {
-        // 접근 제어 로직?
-        return name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public int getAge() {
-        return age;
     }
 }
