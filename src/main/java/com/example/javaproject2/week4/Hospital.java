e54d3305111d610356554a8f1bfe1159b47e738a
Lim ji yang <fluffy8854@naver.com>
2023-05-12 11:47:58 +0900

    feat: ReadFile 수정

--- src/main/java/com/example/javaproject2/week4/Hospital.java
+++ src/main/java/com/example/javaproject2/week4/Hospital.java
@@ -5,6 +5,18 @@ public class Hospital {
     private String name;
     private String phonNumber;
 
+    public Address getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhonNumber() {
+        return phonNumber;
+    }
+
     public Hospital(Address address, String name, String phonNumber) {
         this.address = address;
         this.name = name;
--- src/main/java/com/example/javaproject2/week4/ReadFile.java
+++ src/main/java/com/example/javaproject2/week4/ReadFile.java
@@ -2,6 +2,7 @@ package com.example.javaproject2.week4;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -10,10 +11,29 @@ public class ReadFile {
         ReadFile rf = new ReadFile();
         String fileName = "hospital_info_10lines.csv"; //"C:\\Users\\LJR\\Documents\\3.의료기관별상세정보서비스_01_시설정보_2022.10.UTF8.csv";
         List<String> sl = rf.getLines(fileName);
+        List<Hospital> hl = rf.getHospitals(sl);
         for (int i = 1; i < 10 ; i++) {
-            System.out.println(sl.get(i));
+            System.out.printf("주소 : %s 이름 : %s 홈페이지 : %s\n",hl.get(i).getAddress().getFullAddr(),hl.get(i).getName(),hl.get(i).getPhonNumber());
         }
 
+    }
+
+    public Hospital parse(String str){
+
+        String[] splited = str.split(",");
+
+        Address address = new Address(splited[12],splited[5],splited[7]);
+        Hospital hospital = new Hospital(address,splited[1],splited[11]);
+
+        return hospital;
+    }
+
+    public List<Hospital> getHospitals(List<String> lines){
+        List<Hospital> hospitals = new ArrayList<>();
+        for( String line : lines){
+            hospitals.add(parse(line));
+        }
+        return hospitals;
     }
 
     public List<String> getLines(String fName){
