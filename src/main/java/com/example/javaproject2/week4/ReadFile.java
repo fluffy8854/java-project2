909b10087b38a86b8dd957de4b9109bb65c967c1
Lim ji yang <fluffy8854@naver.com>
2023-05-12 10:28:10 +0900

    feat: ReadFile 수정

--- src/main/java/com/example/javaproject2/week4/ReadFile.java
+++ src/main/java/com/example/javaproject2/week4/ReadFile.java
@@ -8,17 +8,19 @@ import java.util.List;
 public class ReadFile {
     public static void main(String[] args) throws IOException {
         ReadFile rf = new ReadFile();
-        List<String> sl = rf.getLines("C:\\Users\\LJR\\Documents\\3.의료기관별상세정보서비스_01_시설정보_2022.10..csv");
-        for (int i = 0; i < sl.size() ; i++) {
+        String fileName = "hospital_info_10lines.csv"; //"C:\\Users\\LJR\\Documents\\3.의료기관별상세정보서비스_01_시설정보_2022.10.UTF8.csv";
+        List<String> sl = rf.getLines(fileName);
+        for (int i = 1; i < 10 ; i++) {
             System.out.println(sl.get(i));
         }
+
     }
 
     public List<String> getLines(String fName){
         List<String> sl = new ArrayList<>();
 
         try{
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fName),"EUC_KR"));
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fName),"UTF8"));
             String line;
             while((line = br.readLine()) != null){
                 sl.add(line);
