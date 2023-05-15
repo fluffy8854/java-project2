a26d9405560641b48a244e16afb221c88ba1d31d
Lim ji yang <fluffy8854@naver.com>
2023-05-15 09:11:09 +0900

    feat: BubbleSort1 작성

---
+++ src/main/java/com/example/javaproject2/week5/d1/BubbleSort1.java
@@ -0,0 +1,25 @@
+package com.example.javaproject2.week5.d1;
+
+import java.util.Arrays;
+
+public class BubbleSort1 {
+    public static void main(String[] args) {
+        int[] arr = {7,2,3,9,28,11};
+        int tmp;
+
+        tmp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = tmp;
+
+        /*for (int i = 0; i <arr.length -1 ; i++) {
+            for (int j = 0; j < arr.length-1-i ; j++) {
+                if (arr[i] > arr[i + 1]) {
+                    tmp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tmp;
+                }
+            }
+        }*/
+        System.out.println(Arrays.toString(arr));
+    }
+}
