b23257ce660a672dfb790cacf7430ca042c72615
Lim ji yang <fluffy8854@naver.com>
2023-05-15 09:20:46 +0900

    feat: BubbleSort1 수정

--- src/main/java/com/example/javaproject2/week5/d1/BubbleSort1.java
+++ src/main/java/com/example/javaproject2/week5/d1/BubbleSort1.java
@@ -9,10 +9,10 @@ public class BubbleSort1 {
 
         for (int i = 0; i <arr.length -1 ; i++) {
             for (int j = 0; j < arr.length-1-i ; j++) {
-                if (arr[i] > arr[i + 1]) {
-                    tmp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = tmp;
+                if (arr[j] > arr[j + 1]) {
+                    tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
                 }
             }
         }
