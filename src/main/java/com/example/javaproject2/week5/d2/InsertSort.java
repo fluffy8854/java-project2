79a023163c0bd9e3b979e2359e312644f1e78770
Lim ji yang <fluffy8854@naver.com>
2023-05-16 09:34:01 +0900

    feat: InsertSort 수정

--- src/main/java/com/example/javaproject2/week5/d2/InsertSort.java
+++ src/main/java/com/example/javaproject2/week5/d2/InsertSort.java
@@ -26,6 +26,7 @@ public class InsertSort {
         for (int i = 1; i < arr.length; i++) {
             for (int j = i; j >= 1; j--) {
                 iSortARound(arr,j);
+                if(comparator.compare(arr[j-1],arr[j])<=0) break;
             }
         }
     }
