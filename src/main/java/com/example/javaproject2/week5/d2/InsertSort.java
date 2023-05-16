3483041b49dfedb3102e921b69c57f73bc2dac81
Lim ji yang <fluffy8854@naver.com>
2023-05-16 09:19:53 +0900

    feat: InsertSort 수정

--- src/main/java/com/example/javaproject2/week5/d2/InsertSort.java
+++ src/main/java/com/example/javaproject2/week5/d2/InsertSort.java
@@ -5,17 +5,19 @@ import java.util.Comparator;
 
 public class InsertSort implements Comparator<Integer> {
     public static void main(String[] args) {
-        int[] arr = {7,2,3,9,28,11};
+        int[] arr = {7,2,3,9,28,11,1,4,7};
         InsertSort is = new InsertSort();
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = i; j >= 1; j--) {
-                is.iSortARound(arr,j);
-            }
-        }
+        is.iSort(arr);
 
         System.out.println(Arrays.toString(arr));
     }
-
+    public void iSort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j >= 1; j--) {
+                iSortARound(arr,j);
+            }
+        }
+    }
     public void iSortARound(int[]arr, int j){
         if(compare(arr[j-1],arr[j])>0){
             int tmp = arr[j];
@@ -23,7 +25,6 @@ public class InsertSort implements Comparator<Integer> {
             arr[j-1] = tmp;
         }
     }
-
 
     @Override
     public int compare(Integer o1, Integer o2) {
